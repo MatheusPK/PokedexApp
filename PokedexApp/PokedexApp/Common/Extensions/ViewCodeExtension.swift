be0d4d556b949ed25d9e90c2c430a38c4c7fef88
MatheusPK <matheuskulick@hotmail.com>
2022-07-25 08:39:35 -0300

    [ADD] Created ViewCode Protocol

---
+++ PokedexApp/PokedexApp/Common/Extensions/ViewCodeExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ViewCodeExtension.swift
+//  PokedexApp
+//
+//  Created by Matheus P.K on 25/07/22.
+//
+
+import Foundation
+
+extension ViewCode {
+    func setup() {
+        setupComponents()
+        setupConstraints()
+        setupExtraConfiguration()
+    }
+
+    func setupExtraConfiguration() {}
+}
---
+++ PokedexApp/PokedexApp/Common/Protocols/ViewCode.swift
@@ -0,0 +1,15 @@
+//
+//  ViewCodeProtocol.swift
+//  PokedexApp
+//
+//  Created by Matheus P.K on 25/07/22.
+//
+
+import Foundation
+
+protocol ViewCode {
+    func setup()
+    func setupComponents()
+    func setupConstraints()
+    func setupExtraConfiguration()
+}
