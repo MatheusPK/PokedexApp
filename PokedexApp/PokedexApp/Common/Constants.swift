db708aad33d41916722fc986da4a5b9ab98dd856
MatheusPK <matheuskulick@hotmail.com>
2022-07-27 08:42:52 -0300

    [ADD] Added custom fonts

---
+++ PokedexApp/PokedexApp/Common/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  PokedexApp
+//
+//  Created by Matheus P.K on 27/07/22.
+//
+
+import Foundation
+import UIKit
+
+struct K {
+    struct FONTS {
+       static var TITLE = UIFont.systemFont(ofSize: 100, weight: .bold)
+       static var APPLICATION_TITLE = UIFont.systemFont(ofSize: 32, weight: .bold)
+       static var POKEMON_NAME = UIFont.systemFont(ofSize: 26, weight: .bold)
+       static var FILTER_TITLE = UIFont.systemFont(ofSize: 16, weight: .bold)
+       static var DESCRIPTION = UIFont.systemFont(ofSize: 16, weight: .regular)
+       static var POKEMON_NUMBER = UIFont.systemFont(ofSize: 12, weight: .bold)
+       static var POKEMON_TYPE = UIFont.systemFont(ofSize: 12, weight: .medium)
+    }
+}
