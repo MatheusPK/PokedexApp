4b01684555218c2d81f574dde964ef31f6600274
MatheusPK <matheuskulick@hotmail.com>
2022-08-24 10:36:38 -0300

    [ADD] PokemonWeight Model

---
+++ PokedexApp/PokedexApp/Common/Models/PokemonWeight.swift
@@ -0,0 +1,35 @@
+//
+//  PokemonWeight.swift
+//  PokedexApp
+//
+//  Created by Matheus P.K on 24/08/22.
+//
+
+import Foundation
+import UIKit
+
+enum PokemonWeight: Codable, CaseIterable {
+    
+    static var allCases: [PokemonWeight] = [.light(weight: 0), normal(weight: 0), heavy(weight: 0)]
+    
+    case light(weight: Int)
+    case normal(weight: Int)
+    case heavy(weight: Int)
+    
+    func color() -> UIColor {
+        switch self {
+        case .light:  return K.COLORS.WEIGHT.LIGHT
+        case .normal: return K.COLORS.WEIGHT.NORMAL
+        case .heavy:   return K.COLORS.WEIGHT.HEAVY
+        }
+    }
+    
+    func icon() -> UIImage? {
+        switch self {
+        case .light:  return UIImage(named: "Light")
+        case .normal: return UIImage(named: "Normal")
+        case .heavy:   return UIImage(named: "Heavy")
+        }
+    }
+    
+}
